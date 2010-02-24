085e0a8feeba31bffc29e987296ae649e15fa0f6
Joe Smith <yasumoto7@gmail.com>
2010-02-23 23:17:01 -0800

    Finish building grid, finding init and goal. Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- 2/Parser.java
+++ 2/Parser.java
@@ -2,11 +2,17 @@ import java.io.*;
 
 public class Parser {
 	private BufferedReader data;
+	private char[][] arr;
+	private int[] initialPosition;
+	private int[] goalPosition;
+
 	public Parser(String fileName){
 		try {
 			data = new BufferedReader(new FileReader(fileName));
 
 			createGrid();
+			System.out.println("The Initial Position should be at: (" + initialPosition[0] + ", " + initialPosition[1] + ")");
+			System.out.println("The Goal Position should be at: (" + goalPosition[0] + ", " + goalPosition[1] + ")");
 
 			data.close();
                 }
@@ -21,24 +27,47 @@ public class Parser {
 		try {
 			int arraySize = (int)data.read() - '0';
 			int count = 0;
-			//ArrayList al = new ArrayList(arraySize);
+			initialPosition = new int[2];
+			goalPosition = new int[2];
 			System.out.println("The size of the array will be: " + arraySize);
+			arr = new char[arraySize][arraySize];
 
 			//Clear the newline from the size of Array.
 			data.read();
 
-			while (data.ready()) {
-				data.readLine();
-				++count;
+			for(int i = 0; i < arraySize; ++i){
+				for (int j = 0; j < arraySize; ++j){
+					arr[i][j] = (char)data.read();
+					//Determine Initial Position
+					if (arr[i][j] == 'i'){
+						initialPosition[0] = i;
+						initialPosition[1] = j;
+					}
+
+					// Determine Goal Position
+					if (arr[i][j] == 'g'){
+						goalPosition[0] = i;
+						goalPosition[1] = j;
+					}
+
+				}
+				data.read();
 			}
 
-			if (count != arraySize)
-				System.out.println("Error!!! Count is: " + count);
+			// Used for Grid Reading validation
+			for(int i = 0; i < arraySize; ++i){
+				for (int j = 0; j < arraySize; ++j){
+					System.out.print(arr[i][j]);
+				}
+				System.out.println();
+			}
+
 		}
 		
                 catch (IOException e) {
 			System.err.println("Error reading the IOstream");
 			e.printStackTrace();
                 }
+
 	}
 }
